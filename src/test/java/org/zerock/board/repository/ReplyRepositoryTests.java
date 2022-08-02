8c982bec947aec33e1ea59dd2b426359c0ceb97a
yun tasha <jaljayo85@gmail.com>
2022-08-02 18:30:04 +0900

    first commit

---
+++ src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
@@ -0,0 +1,62 @@
+package org.zerock.board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Reply;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReplyRepositoryTests {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void insertReply() {
+
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+            //1부터 100까지의 임의의 번호
+            long bno  = (long)(Math.random() * 100) + 1;
+
+            Board board = Board.builder().bno(bno).build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply......." +i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+
+        });
+
+    }
+
+    @Transactional
+    @Test
+    public void readReply1() {
+
+        Optional<Reply> result = replyRepository.findById(1L);
+
+        Reply reply = result.get();
+
+        System.out.println(reply);
+        System.out.println(reply.getBoard());
+
+    }
+
+
+    @Test
+    public void testListByBoard() {
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(
+                Board.builder().bno(97L).build());
+        replyList.forEach(reply -> System.out.println(reply));
+    }
+
+}
